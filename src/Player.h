33322d9d17284a8a58923a37dfd901c1b4c804f3
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-04-17 23:46:46 -0500

    player cpp

--- src/Player.h
+++ src/Player.h
@@ -1,9 +1,9 @@
 #include "Camera.h"
 #include "Physics.h"
 
-#include "World.h"
-
 #include <GLFW/glfw3.h>
+
+struct World;
 
 struct Player {
   Camera camera;
@@ -12,35 +12,7 @@ struct Player {
   enum class Mode { Survival, Creative, Menger };
   Mode _current_mode = Mode::Creative;
 
-  glm::vec3 acceleration {0};
-
-  void handleTick(const World& world) {
-    // generate impulse to jerk player if collided
-    // if movement resolves collision, no impulse
-    // otherwise impulse
-
-    // if not on ground, tick gravity
-    if (_current_mode == Mode::Survival) {
-      if (not _grounded) {
-        // tick gravity
-        _velocity_y -= 0.5 * 1/60.f;
-
-        // FIXME: cast ray to ground and then ground if reached by current velocity and then place player on ground
-
-        // apply vel_y
-        camera.translate(glm::vec3(0, _velocity_y, 0));
-      }
-      _grounded = grounded(world);
-      if (_grounded) {
-        auto wi = glm::floor(feet());
-        if (world(wi.x, wi.y, wi.z)) {
-          camera.setPos({feet().x, wi.y + 2.25f, feet().z});
-        }
-      }
-    }
-    // FIXME: maybe: if we're collided and grounded bump up
-
-  }
+  void handleTick(const World& world);
 
   void jump() {
     if (_current_mode == Mode::Creative) {
@@ -130,46 +102,11 @@ struct Player {
     }
   }
 
-  bool grounded(const World& world) {
 
-    glm::ivec3 world_index = glm::floor(head());
+  bool grounded(const World& world);
 
-    for (int i = -1; i <= 1; ++i)
-    for (int k = -1; k <= 1; ++k) 
-    for (int j = -3 ; j <= -1; ++j) {
-      glm::ivec3 box = world_index + glm::ivec3(i, j, k);
-            
-      if (not world.isAir(box.x, box.y, box.z)) {
-        if (Physics::verticalCollision(box, feet().y, head().y)
-            && Physics::horizontalCollision(box, head(), 0.5)
-          ) {
-          return true;
-        }
-      }
-    }
+  bool collided(const World& world);
 
-    return false;
-  }
-
-  bool collided(const World& world) {
-
-    glm::ivec3 world_index = blockPosition();
-
-    for (int i = -1; i <= 1; ++i)
-    for (int k = -1; k <= 1; ++k) 
-    for (int j = -5; j <= 1; ++j) {
-      glm::ivec3 box = world_index + glm::ivec3(i, j, k);
-      if (not world.isAir(box.x, box.y, box.z)) {
-        if (Physics::verticalCollision(box, feet().y+0.1, head().y)
-            && Physics::horizontalCollision(box, head(), 0.5))
-        {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
 
   glm::vec3 feet() const {
     return camera.eye() - glm::vec3(0, 1.75, 0);
