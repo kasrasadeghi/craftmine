22f5391879391542bae1f1f9a535c2d6c4c0df5e
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-04-18 16:26:50 -0500

    re-add primitive noise to textures

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -1,15 +1,15 @@
 #include "Terrain.h"
 
 void Terrain::setColors(std::vector<glm::vec4>& base_colors, std::vector<glm::vec4>& off_colors) {
-  base_colors[AIR]   = glm::vec4(0.2, 0.8, 0, 1);
-  base_colors[GRASS] = glm::vec4(0, 1, 0, 1);
+  base_colors[AIR]   = glm::vec4(0);
+  base_colors[GRASS] = glm::vec4(0.2, 0.8, 0, 1);
   base_colors[STONE] = glm::vec4(0.5, 0.5, 0.5, 1);
   base_colors[WATER] = glm::vec4(0, 0, 1, 1);
   base_colors[DIRT]  = glm::vec4(0.5, 0.3, 0, 1);
 
-  off_colors[AIR]   = glm::vec4(0, 0.6, 0, 1);
-  off_colors[GRASS] = glm::vec4(0, 1, 0, 1);
-  off_colors[STONE] = glm::vec4(0.5, 0.5, 0.5, 1);
-  off_colors[WATER] = glm::vec4(0, 0, 1, 1);
-  off_colors[DIRT]  = glm::vec4(0.5, 0.3, 0, 1);
+  off_colors[AIR]   = glm::vec4(0);
+  off_colors[GRASS] = glm::vec4(0, 0.6, 0, 1);
+  off_colors[STONE] = glm::vec4(0.2, 0.2, 0.2, 1);
+  off_colors[WATER] = glm::vec4(0, 0, 0.5, 1);
+  off_colors[DIRT]  = glm::vec4(0.3, 0.1, 0, 1);
 }
