c9b9d1a10ef0d713ef4fc7c8daa63ad41c74ee58
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-04-18 16:28:24 -0500

    make water greenish

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -10,6 +10,6 @@ void Terrain::setColors(std::vector<glm::vec4>& base_colors, std::vector<glm::ve
   off_colors[AIR]   = glm::vec4(0);
   off_colors[GRASS] = glm::vec4(0, 0.6, 0, 1);
   off_colors[STONE] = glm::vec4(0.2, 0.2, 0.2, 1);
-  off_colors[WATER] = glm::vec4(0, 0, 0.5, 1);
+  off_colors[WATER] = glm::vec4(0, 0.5, 0.5, 1);
   off_colors[DIRT]  = glm::vec4(0.3, 0.1, 0, 1);
 }
