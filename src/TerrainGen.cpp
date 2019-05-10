3d2eefe3516be70277c0dc93a1f745982df95a01
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-05-10 18:41:46 -0500

    delete old stuff

--- src/TerrainGen.cpp
+++ src/TerrainGen.cpp
@@ -54,36 +54,9 @@ void TerrainGen::chunk(World& world, glm::ivec2 chunk_index) {
     // solidity of block
     bool column[128] = {}; // all elements zero
 
-    // int height_base = 30 + 50 * perlin(i/ 500.f, k / 500.f);
-
-    // column[height_base] = 1;
-
-    // int base_y = height_base;
-    // float delta_y = 0;
-    // delta_y += 10 * perlin(i / 150.f, k / 150.f);
-    // delta_y = glm::pow(delta_y, 1.15f);
-    // int h = glm::clamp<int>(base_y + delta_y, 0, 127);
-
-    // for (int j = 0; j < 80; ++j) {
-    //   column[j] = j <= h;
-    // }
-
-    // float vertical_variance = 50 + 20 * perlin(i/70.f, k/70.f);
-
     for (int y = 0; y < 128; ++y) {
       if (not column[y]) {
 
-        // float p = perlin(i / 150.f, y / 128.f, k / 150.f);
-        // float scale1 = glm::exp(-0.1 * j + 3/20.f;
-        // float scale = glm::mix(scale0, scale1, 0.2);
-        
-        // column[y] = glm::floor(scale0 * p);
-        // column[y] = glm::floor(scale0);
-        // column[y] = glm::floor(p);
-
-        // float p2 = perlin((i + 100) / 600.f, (k+ 100) / 600.f);
-
-        
         // float gradient =  1 + 1/p2 - y/64.f;
         float p2 = perlin(i / 150.f, 0, k / 150.f);
         float p = perlin(i / 150.f, y / 128.f, k / 150.f);
@@ -92,7 +65,6 @@ void TerrainGen::chunk(World& world, glm::ivec2 chunk_index) {
         float gradient = (2 + p2) - y/64.f;
 
         column[y] = glm::floor(glm::mix(gradient, p, scalefac));
-        // column[y] = glm::floor(scale0);
       }
     }
     
