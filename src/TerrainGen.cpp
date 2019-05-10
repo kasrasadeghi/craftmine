8e58e3721b34fb385bbe0e5b8457e4ead77e69ea
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-05-10 18:34:01 -0500

    make caves use spherical coordinates

--- src/TerrainGen.cpp
+++ src/TerrainGen.cpp
@@ -6,6 +6,8 @@
 
 #include <glm/gtx/hash.hpp>
 #include <glm/gtx/string_cast.hpp>
+#include <glm/gtc/constants.hpp>
+
 #include <iostream>
 #include <unordered_set>
 
@@ -129,18 +131,29 @@ void TerrainGen::chunk(World& world, glm::ivec2 chunk_index) {
   /// Cave generation pass
 
   // randomly pick a point in the chunk
-  if (perlin(bi/100.f, bk/100.f) < 0.2) {
+  if (perlin(bi/15.f, bk/15.f) < 0.2) {
 
-    auto point = glm::vec3(bi + perlin(bi/100.f, bk/100.f) * 15, perlin(bk/100.f, bi/100.f) * 128, bk + perlin(bi/100.f, bk/100.f, (bi ^ bk)/100.f) * 15);
+    auto point = glm::vec3(bi + perlin(bi/15.f, bk/15.f) * 15, perlin(bk/15.f, bi/15.f) * 128, bk + perlin(bi/15.f, bk/15.f, (bi ^ bk)/15.f) * 15);
 
     // map some perlin segments
-    constexpr int point_count = 10;
+    constexpr int point_count = 20;
     std::array<glm::vec3, point_count> cave_points {};
     cave_points[0] = point;
     for (int i = 1; i < point_count; ++i) {
       auto prev = cave_points[i - 1];
-      cave_points[i] = prev + glm::vec3(3) 
-          * glm::vec3(perlin(prev.x, prev.y, prev.z), perlin(prev.z, prev.x, prev.y), perlin(prev.y, prev.z, prev.x));
+
+      float theta = glm::two_pi<float>() * perlin(prev.x / 15.f, prev.y / 15.f, prev.z / 15.f);
+      float phi   = glm::pi<float>() * perlin(prev.x / 15.f, prev.y / 15.f, prev.z / 15.f);
+
+      auto toSpherical = [](float radius, float theta, float phi) -> glm::vec3 {
+        return glm::vec3(
+          radius * glm::cos(theta) * glm::sin(phi),
+          radius * glm::sin(theta) * glm::sin(phi),
+          radius * glm::cos(phi)
+        );
+      };
+
+      cave_points[i] = prev + toSpherical(3, theta, phi);
     }
 
     std::unordered_set<glm::ivec3> carve_voxel_set;
@@ -169,14 +182,16 @@ void TerrainGen::chunk(World& world, glm::ivec2 chunk_index) {
     }
 
     for (glm::ivec3 voxel : carve_voxel_set) {
-      if (voxel.x >= bi && voxel.y >= 0 && voxel.z >= bk
-          && voxel.x < bi + CHUNK_SIZE && voxel.y < CHUNK_HEIGHT && voxel.z < bk + CHUNK_SIZE) {
-        auto& block = world(voxel.x, voxel.y, voxel.z);
-        if (block != Terrain::WATER) {
-          block = Terrain::AIR;
+      if (voxel.y >= 0 && voxel.y < CHUNK_HEIGHT) {
+        if (voxel.x >= bi && voxel.z >= bk
+            && voxel.x < bi + CHUNK_SIZE && voxel.z < bk + CHUNK_SIZE) {
+          auto& block = world(voxel.x, voxel.y, voxel.z);
+          if (block != Terrain::WATER) {
+            block = Terrain::AIR;
+          }
+        } else {
+          cave_voxels_to_be_carved.emplace(voxel);
         }
-      } else {
-        cave_voxels_to_be_carved.emplace(voxel);
       }
     }
   }
