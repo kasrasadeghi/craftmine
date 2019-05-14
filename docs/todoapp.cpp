122c5d6ae5b2133e4bd718390fafa82a79eab6f6
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-05-14 04:52:38 -0500

    init webview todo app with saving!

---
+++ docs/todoapp.cpp
@@ -0,0 +1,33 @@
+#define WEBVIEW_IMPLEMENTATION
+#include "webview.h"
+
+#include <iostream>
+#include <fstream>
+
+typedef struct webview Webview;
+
+void create_webview(Webview& w) {
+  memset(&w, 0, sizeof(Webview));
+  w.title = "Todo";
+  w.url = "file:///home/kasra/projects/minecraft/docs/todoapp.html";
+  w.width = 800;
+  w.height = 600;
+  w.resizable = true;
+  w.external_invoke_cb = [](Webview* w, const char* data) {
+    std::cout << "saving..." << std::endl;
+    std::ofstream out {"todo.json"};
+    out << data;
+    out.close();
+  };
+
+  int r = webview_init(&w);
+  if (r != 0) exit(r);
+}
+
+int main() {
+  Webview w;
+  create_webview(w);
+  while (webview_loop(&w, 1) == 0) ;
+  webview_exit(&w);
+  return 0;
+}
